5c9f084a1a72ec2701c5fc9ab02ed7b136766710
Dzmitry Aliashkevich <dzmitry.aliashkevich@instinctools.ru>
2016-07-06 17:43:31 +0300

    repositories added

--- src/main/java/com/instinctools/utils/CurrencyProviderImpl.java
+++ src/main/java/com/instinctools/utils/CurrencyProviderImpl.java
@@ -5,19 +5,23 @@ import org.springframework.stereotype.Component;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListSet;
 
 /**
  * Created by aldm on 1.7.16.
  */
 @Component
 public class CurrencyProviderImpl implements CurrencyProvider{
+
     @Override
     public Set<Currency> getCurrencies() {
-        Currency usd = new Currency("USD", 0);
-        Currency euro = new Currency("EUR", 0);
+        final Currency usd = new Currency("USD", 0);
+        final Currency euro = new Currency("EUR", 0);
         Set<Currency> currencies = new HashSet<>();
         currencies.add(usd);
         currencies.add(euro);
         return currencies;
     }
+
 }
