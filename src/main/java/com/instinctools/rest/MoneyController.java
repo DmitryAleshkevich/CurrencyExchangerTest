3fd33dd3334f7b51b4f2646487101351c994bae7
Dzmitry Aliashkevich <dzmitry.aliashkevich@instinctools.ru>
2016-07-06 17:43:58 +0300

    repositories added

---
+++ src/main/java/com/instinctools/rest/MoneyController.java
@@ -0,0 +1,17 @@
+package com.instinctools.rest;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by aldm on 6.7.16.
+ */
+
+@RestController
+public class MoneyController {
+
+    private static final Logger logger = LogManager.getLogger(MoneyController.class);
+
+
+}
